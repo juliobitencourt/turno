1c4c6f56a02770fd24c0a9eb7a0f636ab6a66541
Julio Bitencourt <julio@contagia.com.br>
2023-07-13 19:59:45 -0300

    Show message when results are empty

--- resources/views/components/datagrid.blade.php
+++ resources/views/components/datagrid.blade.php
@@ -1,19 +1,23 @@
 <div class="m-6">
     {{ $slot }}
 
-    <div class="divide-y divide-blue-100">
-        @foreach ($list as $item)
-            <div class="py-4 text-xs flex items-center">
-                <div class="flex-1 flex flex-col">
-                    <span class="font-bold">{{ $item['description'] }}</span>
-                    <span class="">{{ $item['date'] }}</span>
+    @if (count($list) === 0)
+        <x-not-found/>
+    @else
+        <div class="divide-y divide-blue-100">
+            @foreach ($list as $item)
+                <div class="py-4 text-xs flex items-center">
+                    <div class="flex-1 flex flex-col">
+                        <span class="font-bold">{{ $item['description'] }}</span>
+                        <span class="">{{ $item['date'] }}</span>
+                    </div>
+                    <div
+                        @class(['text-red-600' => $item['amount'] < 0])
+                    >
+                        {{ $item['amount'] }}
+                    </div>
                 </div>
-                <div
-                    @class(['text-red-600' => $item['amount'] < 0])
-                >
-                    {{ $item['amount'] }}
-                </div>
-            </div>
-        @endforeach
-    </div>
+            @endforeach
+        </div>
+    @endif
 </div>
